cfcdae07664dd0db1bbb4b20da18234e8ea76d80
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 20:39:50 -0300

    refatorado lint do requisito 2 e adicionado requisito 4 do projeto

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  return data.species.some((animais) => animais.name === animal && animais.residents
+  return data.species.find(({ animais }) => animais.name === animal && animais.residents
     .every((idade) => idade.age >= age));
 }
 
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,17 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data
+    .filter((elemento) => elemento.managers.includes(managerId))
+    .map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
