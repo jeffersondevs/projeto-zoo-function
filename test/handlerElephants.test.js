1fd0881799095a5ce2eb36a154e52db2e2ab6dba
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 19:59:15 -0300

    adicionado requisito 6 do projeto

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,14 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para argumento count retorna um numero inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Para argumento names deve retornar um array de nomes que possui o nome que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+});
