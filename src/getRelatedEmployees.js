bb783bfc6991068a9a56ddf0dfb441cc1801eea9
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 20:47:43 -0300

    alterando requisito 4 do projeto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
-  return data.employees.some(({ managers }) => managers.includes(id));
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
@@ -10,7 +12,7 @@ function getRelatedEmployees(managerId) {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  return data
+  return employees
     .filter((elemento) => elemento.managers.includes(managerId))
     .map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
 }
