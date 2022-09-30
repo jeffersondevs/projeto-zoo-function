391bc25d41ba2c5940289dac4d32f9b718219e99
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 21:11:29 -0300

    adicionado requisto 2 do projeto

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  return data.species.find(({ animais }) => animais.name === animal && animais.residents
-    .every((idade) => idade.age >= age));
+  return data.species.find(({ name }) => name === animal).residents
+    .every(({ age: ageAnimal }) => ageAnimal >= age);
 }
 
 module.exports = getAnimalsOlderThan;
