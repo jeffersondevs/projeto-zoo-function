a11b0bcb271aa2a5996296e7a67f9deae7d27ae4
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-30 11:51:53 -0300

    adicionado requisito 15 bonus do projeto

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -22,4 +22,10 @@ describe('Testes da função HandlerElephants', () => {
   it('Não passando argumentos a função deve retornar undefined', () => {
     expect(handlerElephants()).toBeUndefined();
   });
+  it('Passando por argumento um objeto vazio ({}) deve retornar a string Parâmetro inválido, é necessário uma string', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Passada uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('testando')).toBeNull();
+  });
 });
