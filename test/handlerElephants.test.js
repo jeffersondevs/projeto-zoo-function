440889a698a8093e95a3e0796ed9cda5044c2037
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 21:03:16 -0300

    adicionado requisito 7 do projeto

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('Para o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
