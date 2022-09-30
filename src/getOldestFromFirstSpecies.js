1a6cb688636e90f7f0645ffab76b5029870154ca
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-29 21:27:39 -0300

    adicionado requisito 10 do projeto

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const pegarIdAnimal = (id) => data.employees
+  .find((element) => element.id === id)
+  .responsibleFor.map((resp) => resp)[0];
+
+const pegarAntigo = (acc, animal) => {
+  if (acc.age > animal.age) return acc;
+  return animal;
+}
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const IdAnimal = pegarIdAnimal(id);
+  const objeto = data.species.find((element) => element.id === IdAnimal).residents.reduce(pegarAntigo);
+  return Object.keys(objeto).map((element) => objeto[element]);
 }
 
 module.exports = getOldestFromFirstSpecies;
