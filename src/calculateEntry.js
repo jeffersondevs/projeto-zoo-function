8e45a2dd81a31f3d77bffb3ead77b855389d05e8
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-30 11:32:25 -0300

    adcionado requisto 8 do projeto

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqu i
+  // seu código aqui
+  return {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  let soma = 0;
+  const listaEntrada = countEntrants(entrants);
+  soma += (listaEntrada.child * data.prices.child)
+  + (listaEntrada.adult * data.prices.adult)
+  + (listaEntrada.senior * data.prices.senior);
+  return soma;
 }
 
 module.exports = { calculateEntry, countEntrants };
