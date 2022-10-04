c7deb9d818688d6c69f2c38d0a458e4593745211
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-10-04 15:54:46 -0300

    adicionado requisito 5 do projeto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+function countAnimals({ specie, sex } = { specie: 'all', sex: 'all' }) {
   // seu código aqui
+  return data.species.reduce((total, valorAtualEspecie) => {
+    if (specie === 'all') {
+      const animal = { ...total };
+      animal[valorAtualEspecie.name] = valorAtualEspecie.residents.length;
+      return animal;
+    }
+    if (valorAtualEspecie.name === specie) {
+      if (sex) {
+        return valorAtualEspecie.residents
+          .filter(({ sex: sexoAnimal }) => sexoAnimal === sex).length;
+      }
+      return valorAtualEspecie.residents.length;
+    }
+    return total;
+  }, {});
 }
 
 module.exports = countAnimals;
