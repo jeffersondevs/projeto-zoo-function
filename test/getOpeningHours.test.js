4755bbb7ccb54ff9ccb5321dba096e5daa2798a9
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-10-04 15:23:47 -0300

    adicionado requisito 13 do projeto

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,6 +1,18 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
+  it('Teste não passando argumentos. Deverá retornar o objeto:', () => {
+    const expectativa = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(expectativa);
+  });
   it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
     expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
   });
