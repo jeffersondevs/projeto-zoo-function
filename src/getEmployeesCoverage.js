e9f86e0eb6c1cf1be95c37d4aa8aac94d85a7b9d
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-10-04 14:58:38 -0300

    adicionado requisito 11 do projeto

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const findEmployee = (search) => data.employees.find(({ firstName, lastName, id }) =>
+  firstName === search || lastName === search || id === search);
+
+const speciesAndLocation = (responsibleFor) => {
+  const specieNames = [];
+  const locations = [];
+
+  responsibleFor.forEach((employeeSpecieId) => {
+    const animal = data.species.find(({ id: specieId }) => specieId === employeeSpecieId);
+    locations.push(animal.location);
+    specieNames.push(animal.name);
+  });
+
+  return [specieNames, locations];
+};
+
+const createObj = ({ id, firstName, lastName, responsibleFor }) => {
+  const [species, locations] = speciesAndLocation(responsibleFor);
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species,
+    locations,
+  };
+};
+
+function getEmployeesCoverage(searchFor) {
+  if (searchFor) {
+    const [search] = Object.values(searchFor);
+    const employee = findEmployee(search);
+    if (!employee) throw new Error('Informações inválidas');
+    return createObj(employee);
+  }
+  return data.employees.map((employee) => createObj(employee));
 }
 
 module.exports = getEmployeesCoverage;
